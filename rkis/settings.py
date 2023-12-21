8c4dc52dad1dd0270641132f0d07648282dd61ea
sdeix <sdeix2765@gmail.com>
2023-12-21 17:13:57 +0700

    сделаны модели

--- rkis/settings.py
+++ rkis/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'siteapp.apps.SiteappConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'siteapp.User'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- siteapp/models.py
+++ siteapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator, FileExtensionValidator
+from django.forms import forms
+from django.urls import reverse
 
-# Create your models here.
+class User(AbstractUser):
+    name = models.CharField("имя",max_length=20, validators=[RegexValidator(regex='^[а-яА-Я-]*$',message='только ририллица и дефис', code='invalid_username')])
+    def validate_image(value):
+        size_limit = 2*1024*1024
+        if value.size > size_limit:
+            raise forms.ValidationError("размер файла не должен превышать 2Mb")
+    avatar = models.FileField("аватар",upload_to='avatars/',validators=[validate_image,FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'bmp'])])
+    desc = models.TextField("описание",max_length=300)
+    # def get_info(self):
+    #     return reverse('bio',args=[srt(self.id)])
+    class Meta(AbstractUser.Meta):
+        pass
+
+class Post:
+    title = models.CharField('заголовок поста',max_length=30)
+    post_text = models.TextField('текст поста',max_length=300)
+    post_author = models.ForeignKey(User,verbose_name='автор поста', on_delete=models.CASCADE)
+
+class Comment:
+    com_text = models.TextField('текст комментария',max_length=200)
+    com_author = models.ForeignKey(User,verbose_name='автор комментария')
+    com_post = models.ForeignKey(Post,verbose_name="пост")
